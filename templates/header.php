bea2104c47401ae15538a8ed39cf4be423991f99
PN <pn@klapproth-koch.de>
2022-04-06 08:51:38 +0200

    Initial commit

---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<div class="header-banner">
+    <div class="header-banner-top">
+        {{insert_content::50}}
+        {{insert_module::3}}
+    </div>
+    <div class="header-banner-bottom">
+        {{insert_content::53}}
+        <div class="header-banner-bottom-right">
+           <button class="btn-search"><img class="icon-search" src="files/assets/layout/Suche.svg"></button>
+            {{insert_form::1}}
+            {{insert_module::13}}
+        </div>
+
+    </div>
+</div>
---
+++ templates/menu.php
@@ -0,0 +1,26 @@
+<div class="menu-inside">
+<div class="header-banner">
+    <div class="header-banner-top">
+        {{insert_content::50}}
+        {{insert_module::3}}
+    </div>
+    <div class="header-banner-bottom">
+        {{insert_content::53}}
+        <div class="header-banner-bottom-right">
+            <button class="btn-search"><img class="icon-search" src="files/assets/layout/Suche.svg"></button>
+            {{insert_form::1}}
+            <a class="menu-close" href="#" data-mobile-menu="close">×</a>
+        </div>
+    </div>
+</div>
+</div>
+
+<nav class="mod_navigation">
+    <ul class="menu-list">
+        <li>
+           {{insert_module::15}}
+        </li>
+    </ul>
+</nav>
+
+
