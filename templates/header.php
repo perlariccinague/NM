0c398c237eaedde385a8e55734db87d2d4bc497f
PN <pn@klapproth-koch.de>
2022-04-12 12:33:13 +0200

    Neue Änderungen

--- templates/header.php
+++ templates/header.php
@@ -1,15 +1,19 @@
 <div class="header-banner">
     <div class="header-banner-top">
         {{insert_content::50}}
-        {{insert_module::3}}
     </div>
-    <div class="header-banner-bottom">
-        {{insert_content::53}}
+    <div class="header-banner-right">
+        <div class="header-banner-top-right">
+            {{insert_module::3}}
+            {{insert_module::19}}
+        </div>
         <div class="header-banner-bottom-right">
-           <button class="btn-search"><img class="icon-search" src="files/assets/layout/Suche.svg"></button>
+            <button class="btn-search"><img class="icon-search"src="files/assets/layout/Suche.svg"></button>
             {{insert_form::1}}
             {{insert_module::13}}
         </div>
-
+    </div>
+    <div class="header-banner-bottom">
+        {{insert_content::53}}
     </div>
 </div>
